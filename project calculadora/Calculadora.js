6487047ea624aaba00415849f8fac7d96938e76e
Lucas Rodrigues <stormsiders021@gmail.com>
2026-04-01 13:05:26 -0300

    "first commit"

--- project calculadora/Calculadora.js
+++ project calculadora/Calculadora.js
@@ -5,13 +5,15 @@ const prompt = require("prompt-sync")();
 let number = 0;
 let number02 = 0;
 let result = 0;
+let escolha;
 function Adição(number, number02) {
   number = parseFloat(prompt(" Digite um valor "));
   number02 = parseFloat(prompt(" Digite outro valor "));
-  (result = number + number02)
+  result = number + number02
+  console.log(`${number} + ${number02} = ${result}`);
 }
-Adição()
-console.log(result) 
+//Adição()
+//console.log(result) 
 
 
 //divisao
@@ -25,9 +27,10 @@ function Divisão(number03, number04) {
 number03 = parseFloat(prompt(" Digite um numero "))
 number04 = parseFloat(prompt(" Digite outro valor "))
 result = number03 / number04
+console.log(`${number03} / ${number04} = ${result}`);
 }
-Divisão()
-console.log(result)
+//Divisão()
+//console.log(result)
 
 
 //multiplicação
@@ -40,10 +43,10 @@ function multiplicação(number05, number06) {
 number05 = parseFloat(prompt(" digite um valor "))
 number06 = parseFloat(prompt(" digite outro valor "))
 resultmult = number05 * number06
-
+ console.log(`${number05} * ${number06} = ${resultmult}`);
 }
-multiplicação()
-console.log(resultmult)
+//multiplicação()
+//console.log(resultmult)
 
 //subtração
 
@@ -55,9 +58,10 @@ function subtração(number07,number08){
 number07 = parseFloat(prompt(" Digite um valor "))
 number08 = parseFloat(prompt(" Digite outro valor "))
 resultsubt = number07 - number08
+console.log(`${number07} - ${number08} = ${resultsubt}`);
 }
-subtração()
-console.log(resultsubt)
+//subtração()
+//console.log(resultsubt)
 
 
 //porcentagem
@@ -69,13 +73,15 @@ function porcentagem( number09, number10) {
     number09 = parseFloat(prompt(" Digite um valor "))
     number10 = parseFloat(prompt(" Digite outro valor "))
     resultporc = (number09*number10) /100
+    console.log(`${number09} % ${number10} = ${resultporc}`);
 }
-porcentagem()
-console.log(resultporc)
+//porcentagem()
+//console.log(resultporc)
 
-let escolha = 0;
 
-while (escolha != 0) {
+
+
+do {
 console.log("---Seja Bem Vindo a Calculadora---");
 console.log(" Qual opção você deseja usar ")
 console.log(" 1-Adição ")
@@ -84,24 +90,32 @@ console.log(" 3-Multiplicação ")
 console.log(" 4-Subtração ")
 console.log(" 5-Porcentagem ")
 console.log(" 0-Sair")
-let escolha = prompt(" Qual você deseja ? R: ")
-}
+
+
+escolha = parseInt(prompt(" Qual você deseja ? R: "))
+
 switch (escolha) {
     case 1:
         Adição()
-        break;
-case 2:
-    Divisão()
+    break;
+    
+    case 2:
+        Divisão()
+    break;
+    
+    case 3: 
+        multiplicação()
+    break;
 
-case 3: 
-multiplicação()
+    case 4: 
+        subtração()
+    break;
 
-case 4: 
-subtração()
+    case 5: 
+        porcentagem()
+    break;
 
-case 5: 
-porcentagem()
     default: 
-    console.log(" Opção invalida, escolha um valor de 1 a 5 ou 0 para sair ")
+    console.log(" Seleção Indisponivel tente outro número de 1 até 5 ou 0 para sair ")
         break;
-}
+}} while (escolha !== 0);
