73cca9fbccfe0fc4d16b3bd5aa538283d5ce3af9
lr96551 <stormsiders021@gmail.com>
2026-04-08 11:35:17 -0300

    calculadoraest

---
+++ calculadoraestatica /adicionarNumero.js
@@ -0,0 +1,8 @@
+import numeros from "./numeros.js";
+
+function adicionarNumero(num) {
+ numeros.push(num)
+ console.log(" Numero concluído ");    
+}
+
+export default adicionarNumero
---
+++ calculadoraestatica /mediana.js
@@ -0,0 +1,18 @@
+import numeros from "./numeros.js";
+
+function calcularMediana() {
+  let mediana = 0;
+  if (!numeros) {
+    console.log(" Lista de números está vazia! ");
+    return;
+  }
+
+  if (numeros.length % 2 == 0) {
+    mediana =
+      (numeros[numeros.length / 2] + numeros[numeros.length / 2 - 1]) / 2;
+  } else {
+    mediana = numeros.length[Math.floor(numeros.length / 2)];
+  }
+  return mediana;
+}
+export default calcularMediana;
