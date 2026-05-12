8fafbd735a49abed3508d2d5f6adbb7f755d9f16
Apollo2210 <stormsiders021@gmail.com>
2026-05-12 11:54:40 -0300

    "Atualização 120526"

---
+++ modulo_07/app.js
@@ -0,0 +1,18 @@
+import express from 'express';// Chama o Express
+const app = express(); // Cria a lanchonete
+const porta = 3000; // Define o endereço (número da lanchonete)
+
+// ROTA 1: A entrada da lanchonete
+app.get('/', (req, res) => {
+  res.send('Bem-vindo à nossa lanchonete digital!');
+});
+
+// ROTA 2: O cardápio
+app.get('/cardapio', (req, res) => {
+  res.send('Temos Hambúrguer e Suco de Laranja!');
+});
+
+// Liga o servidor
+app.listen(porta, () => {
+  console.log(`Servidor rodando em http://localhost:${porta}`);
+});
