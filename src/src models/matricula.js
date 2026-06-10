e1c4d54ab85dd4bf4d16dfea221cbbb692f0d0ad
Apollo2210 <stormsiders021@gmail.com>
2026-06-10 11:15:54 -0300

    feat: adiciona campos obrigatórios ao esquema de matrícula

--- src/src models/matricula.js
+++ src/src models/matricula.js
@@ -1,3 +1,51 @@
 import mongoose from "mongoose";
 const matriculaSchema = new mongoose.Schema({
-    
+    nomePessoa: {
+type:String,
+required:[true," O nome da Pessoa é obrigatorio "]
+
+    },
+idade: {
+type: Number,
+required:[true, " A idade dessa pessoa é obrigatoria "]
+},
+
+modalidade: {
+
+type: String,
+required:[true, "  A modalidade deve ser obrigatoria "],
+enum: {
+values: [ "Musculação", "Funcional", "Dança" ],
+message: [" Escolha uma das modalidades acima "]
+}
+},
+
+plano: {
+type:String,
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}),
+
